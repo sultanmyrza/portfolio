aa302de7a391f0601b8fb7325eee907384058086
Sultanmyrza Kasymbekov <sultanmyrza@gmail.com>
2020-02-10 21:30:54 +0800

    add skills check list component

---
+++ components/CheckList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>
+      {{ header }}
+    </h2>
+    <ul>
+      <li v-for="item in items" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    header: {
+      type: String,
+      default: () => 'Header'
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+})
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+ul > li::before {
+  content: '✅';
+}
+</style>
